5e5045d59c1660a59edd25eae8edef88307168fb
tracy <tracy@XYSZM00407.lan>
2020-08-16 23:30:54 +0800

    week08作业

--- week08/week08.py
+++ week08/week08.py
@@ -1,12 +1,29 @@
 # coding: utf-8
 """
- 
- @Date: 2020/8/16
- 
- @Author: cuizhen.zhong
- 
- @Copyright（C）: 2014-2018 X-Financial_tmp Inc.   All rights reserved.
- 
- 注意：本内容仅限于小赢科技有限责任公司内部传阅，禁止外泄以及用于其他的商业目的。
- 
+#作业一：
+容器序列: list, tuple, dict
+扁平序列: str, collections.deque
+可变序列: list, dict
+不可变序列: str, tuple, collections.deque
 """
+
+#作业二
+def f_map(func, iter_list):
+    for i in iter_list:
+        yield func(i)
+
+#作业三
+import time
+def timer(func,*args,**kwargs):
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        func(*args,**kwargs)
+        end_time = time.time()
+        print(f'{func.__name__}运行时间: {end_time - start_time}')
+    return wrapper
+
+@timer
+def test1():
+    print('test')
+
+
