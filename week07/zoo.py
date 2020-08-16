77253605585e838b4ec5f41db3525532fd3290ac
tracy <tracy@XYSZM00407.lan>
2020-08-16 23:27:09 +0800

    week08作业

--- week07/zoo.py
+++ week07/zoo.py
@@ -43,21 +43,3 @@ if __name__ == '__main__':
     z.add_animal(cat1)
     # 动物园是否有猫这种动物
     have_cat = getattr(z, 'Cat')
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    # 实例化动物园
-    z = Zoo('时间动物园')
-    # 实例化一只猫，属性包括名字、类型、体型、性格
-    cat1 = Cat('大花猫 1', '食肉', '小', '温顺')
-    # 增加一只猫到动物园
-    z.add_animal(cat1)
-    # 动物园是否有猫这种动物
-    have_cat = getattr(z, 'Cat')
-    print(have_cat)
-
